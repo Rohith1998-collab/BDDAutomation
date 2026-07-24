49b59be2641b5f17a76a2408929f9adcc41d1bd1
rohith <rohithmunukuntla18@gmail.com>
2026-07-24 17:49:09 +0530

    Updated BaseTest driver initialization

--- src/main/java/Base/BaseTest.java
+++ src/main/java/Base/BaseTest.java
@@ -1,8 +1,50 @@
 package Base;
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 	
-private ThreadLocal<WebDriver> driver= new ThreadLocal();
+private static ThreadLocal<WebDriver> driver= new ThreadLocal();
+
+public WebDriver getDriver()
+{
+	
+	return driver.get();
+}
+
+public WebDriver driverInitialization(String browser)
+{
+	WebDriver localDriver=null;
+	
+	switch(browser.toLowerCase())
+	{
+	case "chrome":
+	localDriver= new ChromeDriver();
+	break;
+	case "edge":
+		localDriver= new EdgeDriver();
+		break;
+	case "firefox":
+		localDriver= new FirefoxDriver();
+	break;
+	default:
+		throw new IllegalArgumentException("invalid driver");
+	}
+	localDriver.manage().window().maximize();
+	localDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.set(localDriver);
+	return localDriver;
+	
+
+	
+	
+}
+
+
 	
 	
 
